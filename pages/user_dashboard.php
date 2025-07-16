df71df7d9134d0f9c6934dd606f0c99a2c471d06
Jeneggs <jenexnakaisumo@gmail.com>
2025-07-16 20:36:04 +0800

    Displaying of Positions and Corresponding Candidates

--- pages/user_dashboard.php
+++ pages/user_dashboard.php
@@ -1,9 +1,54 @@
 <?php
 require_once "../config.php";
-if ( isset( $_SESSION['student_number'] ) ) {
-    header("Location: ../login_page.php");
-} else {
-    session_start();
-}
+session_start();
+// if ( isset( $_SESSION['student_number'] ) ) {
+//     header("Location: ../login_page.php");
+//     exit();
+// }
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>user_dashboard</title>
+</head>
+<body>
+    <?php
+    $sql = "SELECT * FROM positions";
+    $stmt = $conn->query($sql);
+    $positions = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    if (count($positions) > 0) {
+        foreach($positions as $position) {
+            echo "<tr><td>". htmlspecialchars($position['position_name']). "</td></tr><br>";
+            try{
+                $sql_2 = "SELECT candidate.* FROM candidate
+                         JOIN positions ON candidate.position_id = positions.id
+                         WHERE candidate.position_id = :position_id";
+                $stmt_2 = $conn->prepare($sql_2);
+                $stmt_2->execute(['position_id' => $position['id']]);
+                $candidates = $stmt_2->fetchAll(PDO::FETCH_ASSOC);
+                if (count($candidates) > 0) {
+                    foreach($candidates as $candidate) {
+                        echo htmlspecialchars($candidate['candidate_name']);
+                    }
+                }
+                else {
+                    echo "No candidates for this position";
+                }
+
+            }
+            catch (PDOException $e) {
+                echo "Error loading candidates";
+            }
+        }
+    }
+    else {
+        echo "No positions available";
+    }
+    
+    ?>
+</body>
+</html>
